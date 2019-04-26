ba7a805d0eb63f6f90a0d089157fe70c7c9a8af2
Lee Crosdale <accounts@leecrosdale.com>
2019-04-26 14:06:58 +0100

    Init

---
+++ i3-setup.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+sudo apt install -y git
+sudo apt install -y libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev libxcb-*-dev autoconf libxcb-xrm0 libxcb-xrm-dev automake
+
+cd /tmp
+
+# clone the repository
+git clone https://www.github.com/Airblader/i3 i3-gaps
+cd i3-gaps
+
+# compile & install
+autoreconf --force --install
+rm -rf build/
+mkdir -p build && cd build/
+
+# Disabling sanitizers is important for release versions!
+# The prefix and sysconfdir are, obviously, dependent on the distribution.
+../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers
+make
+sudo make install
+
+cd
+
+sudo apt install -y i3status suckless-tools
+sudo apt install -y libfftw3-dev libasound2-dev libncursesw5-dev libpulse-dev libtool
+
+git clone https://github.com/karlstav/cava cava
+
+cd cava
+./autogen.sh
+./configure
+make
+sudo make install
+
+cd
+
+sudo apt install -y tizonia
+
+
+
---
+++ run-backup.sh
@@ -0,0 +1,11 @@
+#! bin/bash
+
+cp ~/.config/i3/config ~/i3backup/i3.conf
+cp ~/.config/i3status/config/i3status.conf ~/i3backup/i3status.conf
+cp ~/i3-gaps.sh	~/i3backup/i3-setup.sh
+cp ~/Downloads/lockscreen.png ~/i3backup/images/lockscreen.png
+cp ~/Downloads/tokyo-skyline.png ~/i3backup/images/tokyo-skyline.png
+cp ~/.config/compton.conf ~/i3backup/compton.conf
+cp ~/.Xdefaults ~/i3backup/.Xdefaults
+
+
